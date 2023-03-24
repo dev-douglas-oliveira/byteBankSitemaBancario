f8bb5ee8d93ed9053564d4f68321bff88d7982a9
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-24 16:37:27 -0300

    melhorando o código na herança

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Cliente } from "./Cliente.js"; // importando a classe Cliente
 import { ContaCorrente } from "./ContaCorrente.js"; // importando a classe ContaCorrente
 import { ContaPoupanca } from "./ContaPoupanca.js"; // importando a classe ContaPoupanca
-import { Conta } from "./Conta.js"; // importando a classe Conta
 
 const cliente1 = new Cliente("Ricardo", 11122233309); // criando um objeto do tipo Cliente
 
