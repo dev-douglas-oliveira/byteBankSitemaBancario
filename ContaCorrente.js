4d1e3340650dc8f77b27313ea09e713ef6333d7d
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-24 13:18:50 -0300

    trabalhando com heranças

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,46 +1,8 @@
-import { Cliente } from "./Cliente.js"; // importando a classe Cliente
-
-export class ContaCorrente {
+import { Conta } from "./Conta.js";
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0; //atributo estático para contar o número de contas criadas
-
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            //verifica se o novoValor é uma instância de Cliente
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
-    constructor(agencia, cliente) {
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
-        ContaCorrente.numeroDeContas += 1;
-    }
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    tranferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia); //chamando o construtor da classe pai
+        ContaCorrente.numeroDeContas += 1; //incrementando o número de contas criadas
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { Cliente } from "./Cliente.js"; // importando a classe Cliente
 import { ContaCorrente } from "./ContaCorrente.js"; // importando a classe ContaCorrente
 import { ContaPoupanca } from "./ContaPoupanca.js"; // importando a classe ContaPoupanca
+import { Conta } from "./Conta.js"; // importando a classe Conta
 
 const cliente1 = new Cliente("Ricardo", 11122233309); // criando um objeto do tipo Cliente
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1); // criando um objeto do tipo ContaCorrente
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001); // criando um objeto do tipo ContaCorrente
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
