f353eaecb89614942d250220291fb0ed89763018
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-24 11:54:44 -0300

    criando contatPoupanca.js

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,54 +1,46 @@
-import { Cliente } from "./Cliente.js";
+import { Cliente } from "./Cliente.js"; // importando a classe Cliente
 
-export class ContaCorrente{
-    static numeroDeContas = 0;
-    agencia;
-    _cliente;
-   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-   _saldo = 0;
-    
+export class ContaCorrente {
+    static numeroDeContas = 0; //atributo estático para contar o número de contas criadas
 
-
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            //verifica se o novoValor é uma instância de Cliente
             this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    get saldo(){
+    get saldo() {
         return this._saldo;
     }
 
-    constructor(agencia, cliente){
+    constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
-
-    sacar(valor){
-        if(this._saldo >= valor){
+    sacar(valor) {
+        if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
+    depositar(valor) {
+        if (valor <= 0) {
             return;
-        } 
-        this._saldo += valor;           
+        }
+        this._saldo += valor;
     }
 
-    tranferir(valor, conta){
-        
+    tranferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
     }
 }
